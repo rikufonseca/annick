3b67f007f23a6eb0ac82df06b5211aba062b1eda
Erika Fonseca <fonsecarika@gmail.com>
2022-02-16 21:56:11 +0200

    transition need to be done

--- app/javascript/packs/index-home.js
+++ app/javascript/packs/index-home.js
@@ -1,48 +1,78 @@
 // click on desktop and touch on mobile on the button
-// connecter le button a la page-one et le container
-// when click the triangle left and right open
-// when clicking if page-one is visble make in hidden
 // if page home is hidden make it visible
 // and the page open as well
 
-
 export function home() {
 
+// selectionner tous les elements necessaires
   const elements = document.querySelectorAll(".elements");
   const button = document.getElementById("button");
   const homePage = document.querySelector(".voyages-des-sens");
   const hidePage = document.querySelector(".page-one");
 
+// open triangle left and right
   function moveOne () {
     elements.forEach(element => {
       element.classList.toggle('open')
     })
   };
 
+// make page one hidden gradually
   function moveTwoDelay () {
     function moveTwo () {
       if (hidePage.classList.contains('hidden')) {
         hidePage.classList.remove('hidden');
         setTimeout(function () {
           hidePage.classList.remove('visuallyhidden');
-        }, 20);
+        }, 10);
       } else {
         hidePage.classList.add('visuallyhidden');
         hidePage.addEventListener('transitioned', function (e){
           hidePage.classList.add('hidden');
-        }, {
-          capture: false,
-          once: true,
-          passive: false
         });
       }
     };
     setTimeout(moveTwo, 1000);
   };
 
+//move page-one in html down to voyages-des-sens page
+  function moveDownDelay () {
+    function moveDownDiv () {
+     const here = document.querySelector('.here');
+     here.innerHTML = "";
+    //  const pageOne = here.children;
+    //  const there = document.createElement("div");
+    //  there.className = "there";
+    //  there.innerHTML = pageOne;
+    //  document.body.appendChild(there);
+    };
+    setTimeout(moveDownDiv, 1100);
+  };
+
+//make voyages-des-sens show up gradually
+  function moveThreeDelay() {
+    function moveThree() {
+      if (homePage.classList.contains('show')) {
+        homePage.classList.remove('show');
+        setTimeout(function () {
+          homePage.classList.remove('visuallyshow');
+        });
+      } else {
+        homePage.classList.add('visuallyshow');
+        homePage.addEventListener('transitioned', function (e) {
+          homePage.classList.add('show');
+        }, 10);
+      }
+    };
+    setTimeout(moveThree, 1000);
+  };
+
+//get all the moves and connect it to the button
  const ready = function allMoves () {
                 moveOne();
                 moveTwoDelay();
+                moveDownDelay();
+                moveThreeDelay();
 }
 
   button.addEventListener("click", ready, false );
