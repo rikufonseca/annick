bf224ec3c70b51867986dec9d3d4999876640cb6
Erika Fonseca <fonsecarika@gmail.com>
2022-02-16 16:30:26 +0200

    transition almost done of home page only need the fadein

--- app/javascript/packs/index-home.js
+++ app/javascript/packs/index-home.js
@@ -1,19 +1,49 @@
 // click on desktop and touch on mobile on the button
 // connecter le button a la page-one et le container
 // when click the triangle left and right open
-//display voyage des sens div
+// when clicking if page-one is visble make in hidden
+// if page home is hidden make it visible
 // and the page open as well
 
-//classList.toggle('open')page.classList.toggle('open')
- // ("click", e => {...}, { capture: true})
+
 export function home() {
 
-  let elements = document.querySelectorAll(".elements");
-  let button = document.getElementById("button");
+  const elements = document.querySelectorAll(".elements");
+  const button = document.getElementById("button");
+  const homePage = document.querySelector(".voyages-des-sens");
+  const hidePage = document.querySelector(".page-one");
 
-  button.addEventListener("click", () => {
+  function moveOne () {
     elements.forEach(element => {
       element.classList.toggle('open')
     })
-  });
+  };
+
+  function moveTwoDelay () {
+    function moveTwo () {
+      if (hidePage.classList.contains('hidden')) {
+        hidePage.classList.remove('hidden');
+        setTimeout(function () {
+          hidePage.classList.remove('visuallyhidden');
+        }, 20);
+      } else {
+        hidePage.classList.add('visuallyhidden');
+        hidePage.addEventListener('transitioned', function (e){
+          hidePage.classList.add('hidden');
+        }, {
+          capture: false,
+          once: true,
+          passive: false
+        });
+      }
+    };
+    setTimeout(moveTwo, 1000);
+  };
+
+ const ready = function allMoves () {
+                moveOne();
+                moveTwoDelay();
+}
+
+  button.addEventListener("click", ready, false );
 }
