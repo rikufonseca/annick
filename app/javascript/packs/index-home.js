166c900850922cb567b09c7d8921aeedeb81a6fe
Erika Fonseca <fonsecarika@gmail.com>
2022-02-17 13:21:13 +0200

    pb with user sheet agent

--- app/javascript/packs/index-home.js
+++ app/javascript/packs/index-home.js
@@ -7,7 +7,7 @@ export function home() {
 // selectionner tous les elements necessaires
   const elements = document.querySelectorAll(".elements");
   const button = document.getElementById("button");
-  const homePage = document.querySelector(".voyages-des-sens");
+  const homePage = document.getElementById("voyages-des-sens");
   const hidePage = document.querySelector(".page-one");
 
 // open triangle left and right
@@ -18,62 +18,36 @@ export function home() {
   };
 
 // make page one hidden gradually
-  function moveTwoDelay () {
-    function moveTwo () {
-     // if (hidePage.classList.contains('hidden')) {
-     //   hidePage.classList.remove('hidden');
-     //   setTimeout(function () {
-      //    hidePage.classList.remove('visuallyhidden');
-    //    }, 10);
-    //  } else {
-        hidePage.classList.add('visuallyhidden');
-        hidePage.addEventListener('transitioned', function (e){
-          hidePage.classList.add('hidden');
-        }, 20);
-   //   }
+  function moveTwoDelay() {
+    function moveTwo() {
+      hidePage.classList.add('visuallyhidden');
+      hidePage.addEventListener('transitioned', function (e) {
+        hidePage.classList.add('hidden');
+      }, 5);
     };
-    setTimeout(moveTwo, 2000);
-  };
-
-//move page-one in html down to voyages-des-sens page
-  function moveDownDelay () {
-    function moveDownDiv () {
-     const here = document.querySelector('.here');
-    // here.innerHTML = "";
-     const pageOne = here.children;
-     const there = document.createElement("div");
-      there.className = "there";
-      there.innerHTML = pageOne;
-    //  document.body.appendChild(there);
-    };
-    setTimeout(moveDownDiv, 1100);
+    setTimeout(moveTwo, 1000);
   };
 
 //make voyages-des-sens show up gradually
   function moveThreeDelay() {
     function moveThree() {
-      if (homePage.classList.contains('show')) {
-        homePage.classList.remove('show');
-        setTimeout(function () {
-          homePage.classList.remove('visuallyshow');
-        });
-      } else {
+      if (hidePage.classList.contains('visuallyhidden')) {
         homePage.classList.add('visuallyshow');
         homePage.addEventListener('transitioned', function (e) {
-          homePage.classList.add('show');
-        }, 10);
-      }
-    };
-    setTimeout(moveThree, 1000);
+         homePage.classList.add('slow-process')
+        }, 20);
+      };
+    }
+    setTimeout(moveThree, 2000);
   };
 
-//get all the moves and connect it to the button
- const ready = function allMoves () {
-                moveOne();
-                moveTwoDelay();
-                moveDownDelay();
-                moveThreeDelay();
-}
+    //get all the moves and connect it to the button
+  const ready = function allMoves () {
+                  moveOne();
+                  moveTwoDelay();
+              //    moveDownDelay();
+                  moveThreeDelay();
+                }
 
   button.addEventListener("click", ready, false );
 }
